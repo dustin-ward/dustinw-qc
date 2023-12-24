6d95ed7c380c00c5672f0cff66ff5954eeb8fdc4
dustin-ward <wardddustin@gmail.com>
2023-12-23 20:57:34 -0500

    lexer tests

--- src/lexer.rs
+++ src/lexer.rs
@@ -26,6 +26,7 @@ pub struct Token {
     t: TokenType,
     line: u32,
     pos: u32,
+    len: usize,
 }
 
 // Read file by line from:
@@ -59,6 +60,7 @@ pub fn tokenize(filename: &str) -> Result<Vec<Token>, String> {
                         t: TokenType::UNDEF,
                         line: lineno as u32 + 1,
                         pos : pos as u32 + 1,
+                        len: 1
                     };
 
                     new_token.t = match ch {
@@ -72,6 +74,7 @@ pub fn tokenize(filename: &str) -> Result<Vec<Token>, String> {
                                 }
 
                                 let (_, next_ch) = iter.next().unwrap();
+                                new_token.len += 1;
                                 ident.push(next_ch);
                             }
 
@@ -98,6 +101,7 @@ pub fn tokenize(filename: &str) -> Result<Vec<Token>, String> {
                                 if next_ch == '.' {
                                     is_float = true;
                                 }
+                                new_token.len += 1;
                                 ident.push(next_ch);
                             }
 
@@ -119,7 +123,7 @@ pub fn tokenize(filename: &str) -> Result<Vec<Token>, String> {
                     };
 
                     if new_token.t == TokenType::UNDEF {
-                        return Err(format!("Undefined token at {}:{} \"{}\"", lineno+1, pos+1, &line[pos..pos+1]));
+                        return Err(format!("Undefined token at {}:{} \"{}\"", lineno+1, pos+1, &line[pos..pos+new_token.len]));
                     }
 
                     tokens.push(new_token);
@@ -132,6 +136,7 @@ pub fn tokenize(filename: &str) -> Result<Vec<Token>, String> {
                             t: TokenType::EOL,
                             line: lineno as u32 + 1,
                             pos: line.len() as u32 + 1,
+                            len: 1,
                         });
                     }
                 }
@@ -148,32 +153,35 @@ pub fn tokenize(filename: &str) -> Result<Vec<Token>, String> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    const TESTDATA_DIR: &str = "examples/testdata";
+
+    // General Tests
 
     #[test]
     fn get_numeric_tokens() {
         let expected_tokens = vec![
-            Token{t: TokenType::Float(1.234), line: 1, pos: 1},
-            Token{t: TokenType::EOL, line: 1, pos: 6},
-            Token{t: TokenType::Integer(0), line: 2, pos: 1},
-            Token{t: TokenType::EOL, line: 2, pos: 2},
-            Token{t: TokenType::Float(12345.6789), line: 3, pos: 1},
-            Token{t: TokenType::EOL, line: 3, pos: 11},
-            Token{t: TokenType::Float(1.), line: 4, pos: 1},
-            Token{t: TokenType::EOL, line: 4, pos: 3},
-            Token{t: TokenType::Float(0.0), line: 5, pos: 1},
-            Token{t: TokenType::EOL, line: 5, pos: 4},
-            Token{t: TokenType::Integer(1), line: 6, pos: 1},
-            Token{t: TokenType::EOL, line: 6, pos: 2},
-            Token{t: TokenType::Integer(2000000000), line: 7, pos: 1},
-            Token{t: TokenType::EOL, line: 7, pos: 11},
-            Token{t: TokenType::Float(0.0001), line: 8, pos: 1},
-            Token{t: TokenType::EOL, line: 8, pos: 7},
-            Token{t: TokenType::Integer(12345), line: 9, pos: 1},
-            Token{t: TokenType::EOL, line: 9, pos: 6},
+            Token{t: TokenType::Float(1.234), line: 1, pos: 1, len: 5},
+            Token{t: TokenType::EOL, line: 1, pos: 6, len: 1},
+            Token{t: TokenType::Integer(0), line: 2, pos: 1, len: 1},
+            Token{t: TokenType::EOL, line: 2, pos: 2, len: 1},
+            Token{t: TokenType::Float(12345.6789), line: 3, pos: 1, len: 10},
+            Token{t: TokenType::EOL, line: 3, pos: 11, len: 1},
+            Token{t: TokenType::Float(1.), line: 4, pos: 1, len: 2},
+            Token{t: TokenType::EOL, line: 4, pos: 3, len: 1},
+            Token{t: TokenType::Float(0.0), line: 5, pos: 1, len: 3},
+            Token{t: TokenType::EOL, line: 5, pos: 4, len: 1},
+            Token{t: TokenType::Integer(1), line: 6, pos: 1, len: 1},
+            Token{t: TokenType::EOL, line: 6, pos: 2, len: 1},
+            Token{t: TokenType::Integer(2000000000), line: 7, pos: 1, len: 10},
+            Token{t: TokenType::EOL, line: 7, pos: 11, len: 1},
+            Token{t: TokenType::Float(0.0001), line: 8, pos: 1, len: 6},
+            Token{t: TokenType::EOL, line: 8, pos: 7, len: 1},
+            Token{t: TokenType::Integer(12345), line: 9, pos: 1, len: 5},
+            Token{t: TokenType::EOL, line: 9, pos: 6, len: 1},
         ];
 
-        let test_filename = "examples/testdata/get_numeric_tokens.testdata";
-        let actual_tokens = tokenize(test_filename).unwrap();
+        let test_filename = format!("{TESTDATA_DIR}/get_numeric_tokens.testdata");
+        let actual_tokens = tokenize(&test_filename).unwrap();
 
         assert_eq!(expected_tokens.len(), actual_tokens.len());
 
@@ -184,26 +192,37 @@ mod tests {
 
     #[test]
     fn invalid_numeric_tokens() {
-        let test_filename = "examples/testdata/invalid_numeric_tokens1.testdata";
-        let err = tokenize(test_filename).unwrap_err();
+        // .123456
+        let mut test_filename = format!("{TESTDATA_DIR}/invalid_numeric_tokens1.testdata");
+        let mut err = tokenize(&test_filename).unwrap_err();
         assert_eq!(err, "Undefined token at 1:1 \".\"");
+        
+        // 0.a132
+        test_filename = format!("{TESTDATA_DIR}/invalid_numeric_tokens2.testdata");
+        err = tokenize(&test_filename).unwrap_err();
+        assert_eq!(err, "Undefined token at 1:3 \"a\"");
+
+        // 1,000,000
+        test_filename = format!("{TESTDATA_DIR}/invalid_numeric_tokens3.testdata");
+        err = tokenize(&test_filename).unwrap_err();
+        assert_eq!(err, "Undefined token at 1:2 \",\"");
     }
 
     #[test]
     fn get_function_tokens() {
         let expected_tokens = vec![
-            Token{t: TokenType::RX, line: 1, pos: 1},
-            Token{t: TokenType::EOL, line: 1, pos: 3},
-            Token{t: TokenType::RZ, line: 2, pos: 1},
-            Token{t: TokenType::EOL, line: 2, pos: 3},
-            Token{t: TokenType::CZ, line: 3, pos: 1},
-            Token{t: TokenType::EOL, line: 3, pos: 3},
-            Token{t: TokenType::MEASURE, line: 4, pos: 1},
-            Token{t: TokenType::EOL, line: 4, pos: 8},
+            Token{t: TokenType::RX, line: 1, pos: 1, len: 2},
+            Token{t: TokenType::EOL, line: 1, pos: 3, len: 1},
+            Token{t: TokenType::RZ, line: 2, pos: 1, len: 2},
+            Token{t: TokenType::EOL, line: 2, pos: 3, len: 1},
+            Token{t: TokenType::CZ, line: 3, pos: 1, len: 2},
+            Token{t: TokenType::EOL, line: 3, pos: 3, len: 1},
+            Token{t: TokenType::MEASURE, line: 4, pos: 1, len: 7},
+            Token{t: TokenType::EOL, line: 4, pos: 8, len: 1},
         ];
 
-        let test_filename = "examples/testdata/get_function_tokens.testdata";
-        let actual_tokens = tokenize(test_filename).unwrap();
+        let test_filename = format!("{TESTDATA_DIR}/get_function_tokens.testdata");
+        let actual_tokens = tokenize(&test_filename).unwrap();
 
         assert_eq!(expected_tokens.len(), actual_tokens.len());
 
@@ -213,22 +232,169 @@ mod tests {
     }
     
     #[test]
+    fn invalid_function_tokens() {
+        // rx(0.0) 0
+        let mut test_filename = format!("{TESTDATA_DIR}/invalid_function_tokens1.testdata");
+        let mut err = tokenize(&test_filename).unwrap_err();
+        assert_eq!(err, "Undefined token at 1:1 \"rx\"");
+        
+        // RAX(1.0) 1
+        test_filename = format!("{TESTDATA_DIR}/invalid_function_tokens2.testdata");
+        err = tokenize(&test_filename).unwrap_err();
+        assert_eq!(err, "Undefined token at 1:1 \"RAX\"");
+    }
+
+    #[test]
     fn get_misc_tokens() {
         let expected_tokens = vec![
-            Token{t: TokenType::LParen, line: 1, pos: 1},
-            Token{t: TokenType::EOL, line: 1, pos: 2},
-            Token{t: TokenType::RParen, line: 1, pos: 3},
-            Token{t: TokenType::EOL, line: 1, pos: 4},
-            Token{t: TokenType::Negative, line: 2, pos: 1},
-            Token{t: TokenType::LParen, line: 2, pos: 2},
-            Token{t: TokenType::EOL, line: 2, pos: 3},
-            Token{t: TokenType::EOL, line: 3, pos: 1},
-            Token{t: TokenType::RParen, line: 3, pos: 2},
-            Token{t: TokenType::EOL, line: 3, pos: 3},
+            Token{t: TokenType::LParen, line: 1, pos: 1, len: 1},
+            Token{t: TokenType::EOL, line: 1, pos: 2, len: 1},
+            Token{t: TokenType::RParen, line: 1, pos: 3, len: 1},
+            Token{t: TokenType::EOL, line: 1, pos: 4, len: 1},
+            Token{t: TokenType::Negative, line: 2, pos: 1, len: 1},
+            Token{t: TokenType::LParen, line: 2, pos: 2, len: 1},
+            Token{t: TokenType::EOL, line: 2, pos: 3, len: 1},
+            Token{t: TokenType::EOL, line: 3, pos: 1, len: 1},
+            Token{t: TokenType::RParen, line: 3, pos: 2, len: 1},
+            Token{t: TokenType::EOL, line: 3, pos: 3, len: 1},
         ];
 
-        let test_filename = "examples/testdata/get_misc_tokens.testdata";
-        let actual_tokens = tokenize(test_filename).unwrap();
+        let test_filename = format!("{TESTDATA_DIR}/get_misc_tokens.testdata");
+        let actual_tokens = tokenize(&test_filename).unwrap();
+
+        assert_eq!(expected_tokens.len(), actual_tokens.len());
+
+        for (i, ex_token) in expected_tokens.iter().enumerate() {
+            assert_eq!(ex_token, &actual_tokens[i]);
+        }
+    }
+
+    // Sample Input Tests
+
+    #[test]
+    fn tokenize_sample_1() {
+        let expected_tokens = vec![
+            Token{t: TokenType::RX, line: 1, pos: 1, len: 2},
+            Token{t: TokenType::LParen, line: 1, pos: 3, len: 1},
+            Token{t: TokenType::Float(0.45), line: 1, pos: 4, len: 4},
+            Token{t: TokenType::RParen, line: 1, pos: 8, len: 1},
+            Token{t: TokenType::Integer(0), line: 1, pos: 10, len: 1},
+            Token{t: TokenType::EOL, line: 1, pos: 11, len: 1},
+        ];
+
+        let test_filename = format!("{TESTDATA_DIR}/sample_1.quil");
+        let actual_tokens = tokenize(&test_filename).unwrap();
+
+        assert_eq!(expected_tokens.len(), actual_tokens.len());
+
+        for (i, ex_token) in expected_tokens.iter().enumerate() {
+            assert_eq!(ex_token, &actual_tokens[i]);
+        }
+    }
+
+    #[test]
+    fn tokenize_sample_2() {
+        let expected_tokens = vec![
+            Token{t: TokenType::RX, line: 1, pos: 1, len: 2},
+            Token{t: TokenType::LParen, line: 1, pos: 3, len: 1},
+            Token{t: TokenType::Float(0.45), line: 1, pos: 4, len: 4},
+            Token{t: TokenType::RParen, line: 1, pos: 8, len: 1},
+            Token{t: TokenType::Integer(0), line: 1, pos: 10, len: 1},
+            Token{t: TokenType::EOL, line: 1, pos: 11, len: 1},
+
+            Token{t: TokenType::RZ, line: 2, pos: 1, len: 2},
+            Token{t: TokenType::LParen, line: 2, pos: 3, len: 1},
+            Token{t: TokenType::Negative, line: 2, pos: 4, len: 1},
+            Token{t: TokenType::Float(1.0), line: 2, pos: 5, len: 3},
+            Token{t: TokenType::RParen, line: 2, pos: 8, len: 1},
+            Token{t: TokenType::Integer(0), line: 2, pos: 10, len: 1},
+            Token{t: TokenType::EOL, line: 2, pos: 11, len: 1},
+
+            Token{t: TokenType::RZ, line: 3, pos: 1, len: 2},
+            Token{t: TokenType::LParen, line: 3, pos: 3, len: 1},
+            Token{t: TokenType::Float(1.0), line: 3, pos: 4, len: 3},
+            Token{t: TokenType::RParen, line: 3, pos: 7, len: 1},
+            Token{t: TokenType::Integer(1), line: 3, pos: 9, len: 1},
+            Token{t: TokenType::EOL, line: 3, pos: 10, len: 1},
+
+            Token{t: TokenType::RX, line: 4, pos: 1, len: 2},
+            Token{t: TokenType::LParen, line: 4, pos: 3, len: 1},
+            Token{t: TokenType::Float(0.45), line: 4, pos: 4, len: 4},
+            Token{t: TokenType::RParen, line: 4, pos: 8, len: 1},
+            Token{t: TokenType::Integer(1), line: 4, pos: 10, len: 1},
+            Token{t: TokenType::EOL, line: 4, pos: 11, len: 1},
+            
+            Token{t: TokenType::CZ, line: 5, pos: 1, len: 2},
+            Token{t: TokenType::Integer(0), line: 5, pos: 4, len: 1},
+            Token{t: TokenType::Integer(1), line: 5, pos: 6, len: 1},
+            Token{t: TokenType::EOL, line: 5, pos: 7, len: 1},
+
+            Token{t: TokenType::MEASURE, line: 6, pos: 1, len: 7},
+            Token{t: TokenType::Integer(0), line: 6, pos: 9, len: 1},
+            Token{t: TokenType::EOL, line: 6, pos: 10, len: 1},
+
+            Token{t: TokenType::MEASURE, line: 7, pos: 1, len: 7},
+            Token{t: TokenType::Integer(1), line: 7, pos: 9, len: 1},
+            Token{t: TokenType::EOL, line: 7, pos: 10, len: 1},
+        ];
+
+        let test_filename = format!("{TESTDATA_DIR}/sample_2.quil");
+        let actual_tokens = tokenize(&test_filename).unwrap();
+
+        assert_eq!(expected_tokens.len(), actual_tokens.len());
+
+        for (i, ex_token) in expected_tokens.iter().enumerate() {
+            assert_eq!(ex_token, &actual_tokens[i]);
+        }
+    }
+
+    #[test]
+    fn tokenize_sample_3() {
+        let expected_tokens = vec![
+            Token{t: TokenType::RX, line: 1, pos: 1, len: 2},
+            Token{t: TokenType::LParen, line: 1, pos: 3, len: 1},
+            Token{t: TokenType::Float(0.45), line: 1, pos: 4, len: 4},
+            Token{t: TokenType::RParen, line: 1, pos: 8, len: 1},
+            Token{t: TokenType::Integer(0), line: 1, pos: 10, len: 1},
+            Token{t: TokenType::EOL, line: 1, pos: 11, len: 1},
+
+            Token{t: TokenType::RZ, line: 2, pos: 1, len: 2},
+            Token{t: TokenType::LParen, line: 2, pos: 3, len: 1},
+            Token{t: TokenType::Negative, line: 2, pos: 4, len: 1},
+            Token{t: TokenType::Float(1.0), line: 2, pos: 5, len: 3},
+            Token{t: TokenType::RParen, line: 2, pos: 8, len: 1},
+            Token{t: TokenType::Integer(0), line: 2, pos: 10, len: 1},
+            Token{t: TokenType::EOL, line: 2, pos: 11, len: 1},
+
+            Token{t: TokenType::RZ, line: 3, pos: 1, len: 2},
+            Token{t: TokenType::LParen, line: 3, pos: 3, len: 1},
+            Token{t: TokenType::Float(1.0), line: 3, pos: 4, len: 3},
+            Token{t: TokenType::RParen, line: 3, pos: 7, len: 1},
+            Token{t: TokenType::Integer(1), line: 3, pos: 9, len: 1},
+            Token{t: TokenType::EOL, line: 3, pos: 10, len: 1},
+
+            Token{t: TokenType::RX, line: 4, pos: 1, len: 2},
+            Token{t: TokenType::LParen, line: 4, pos: 3, len: 1},
+            Token{t: TokenType::Float(0.45), line: 4, pos: 4, len: 4},
+            Token{t: TokenType::RParen, line: 4, pos: 8, len: 1},
+            Token{t: TokenType::Integer(1), line: 4, pos: 10, len: 1},
+            Token{t: TokenType::EOL, line: 4, pos: 11, len: 1},
+            
+            Token{t: TokenType::CZ, line: 5, pos: 1, len: 2},
+            Token{t: TokenType::Integer(1), line: 5, pos: 4, len: 1},
+            Token{t: TokenType::Integer(0), line: 5, pos: 6, len: 1},
+            Token{t: TokenType::EOL, line: 5, pos: 7, len: 1},
+
+            Token{t: TokenType::RZ, line: 6, pos: 1, len: 2},
+            Token{t: TokenType::LParen, line: 6, pos: 3, len: 1},
+            Token{t: TokenType::Float(1.5707963267948966), line: 6, pos: 4, len: 18},
+            Token{t: TokenType::RParen, line: 6, pos: 22, len: 1},
+            Token{t: TokenType::Integer(1), line: 6, pos: 24, len: 1},
+            Token{t: TokenType::EOL, line: 6, pos: 25, len: 1},
+        ];
+
+        let test_filename = format!("{TESTDATA_DIR}/sample_3.quil");
+        let actual_tokens = tokenize(&test_filename).unwrap();
 
         assert_eq!(expected_tokens.len(), actual_tokens.len());
 
