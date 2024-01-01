b57111e1c90a80402e95597904554cbff20f4870
dustin-ward <wardddustin@gmail.com>
2024-01-01 14:00:43 -0500

    loop optimizations

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ use dustinw_qc::lexer;
 use dustinw_qc::parser;
 use dustinw_qc::parser::Instruction;
 
+use dustinw_qc::optimize::cz_cancel;
 use dustinw_qc::optimize::deadcode;
 use dustinw_qc::optimize::native_translation;
-use dustinw_qc::optimize::rotation_merge;
-use dustinw_qc::optimize::cz_cancel;
 use dustinw_qc::optimize::reorder;
+use dustinw_qc::optimize::rotation_merge;
+
+const MAX_OP_PASSES: u32 = 100;
 
 fn main() {
     // Parse Args
@@ -36,35 +38,43 @@ fn main() {
     }
     let mut program: Vec<Instruction> = program_result.unwrap();
 
+    // Translate program to native instructions only
+    let translation_result = native_translation::native_translation_pass(program);
+    if let Err(err) = translation_result {
+        println!("native instruction translation: {}", err);
+        process::exit(1);
+    }
+    program = translation_result.unwrap();
+
     // Code passes
     let code_passes: Vec<(
         &str,
         fn(Vec<Instruction>) -> Result<Vec<Instruction>, String>,
     )> = vec![
-        (
-            "native_translation",
-            native_translation::native_translation_pass,
-        ),
-        ("deadcode1", deadcode::deadcode_pass),
-        ("rotation_merge1", rotation_merge::rotation_merge_pass),
-        ("cz_cancel1", cz_cancel::cz_cancel_pass),
-        ("reorder1", reorder::reorder_pass),
-        ("deadcode2", deadcode::deadcode_pass),
-        ("rotation_merge2", rotation_merge::rotation_merge_pass),
-        ("cz_cancel2", cz_cancel::cz_cancel_pass),
-        ("reorder2", reorder::reorder_pass),
-        ("deadcode3", deadcode::deadcode_pass),
-        ("rotation_merge3", rotation_merge::rotation_merge_pass),
-        ("cz_cancel3", cz_cancel::cz_cancel_pass),
-        ("deadcode4", deadcode::deadcode_pass),
+        ("reorder", reorder::reorder_pass),
+        ("rotation_merge", rotation_merge::rotation_merge_pass),
+        ("cz_cancel", cz_cancel::cz_cancel_pass),
+        ("deadcode", deadcode::deadcode_pass),
     ];
-    for (name, pass_func) in code_passes {
-        match pass_func(program) {
-            Ok(new_prog) => program = new_prog,
-            Err(err) => {
-                println!("{}: {}", name, err);
-                process::exit(1);
+
+    let mut prog_len = program.len();
+    for _round in 0..MAX_OP_PASSES {
+        // Perform optimization passes
+        for (name, pass_func) in &code_passes {
+            match pass_func(program) {
+                Ok(new_prog) => program = new_prog,
+                Err(err) => {
+                    println!("{}: {}", name, err);
+                    process::exit(1);
+                }
             }
+        }
+
+        // Terminate optimization if program length is not changing
+        if program.len() == prog_len {
+            break;
+        } else {
+            prog_len = program.len();
         }
     }
 
